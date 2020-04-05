e6bda2ade0b62f3229ca3d9035bf64f416d443e2
Bustamante Jonathan <JBustamante@luxotticaretail.com>
2020-04-04 22:28:00 -0400

    Reading request from chatfuel

--- cp-rest/src/main/java/com/cpchatbot/rest/controller/ChatbotController.java
+++ cp-rest/src/main/java/com/cpchatbot/rest/controller/ChatbotController.java
@@ -3,18 +3,20 @@ package com.cpchatbot.rest.controller;
 import com.cpchatbot.rest.model.Message;
 import com.cpchatbot.rest.model.Response;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.Map;
 
 @RestController
 public class ChatbotController {
 
     @PostMapping(path = "hello")
-    public Response helloWorld(){
+    public Response helloWorld(@RequestBody Map<String, ?> input){
         Response response = new Response();
         Message m= new Message();
-        m.text = "Hello World";
+        m.text = "You Are "+input.get("first name");
         response.messages = Arrays.asList(m);
         return response;
     }
---
+++ cp-rest/src/main/java/com/cpchatbot/rest/model/Request.java
@@ -0,0 +1,5 @@
+package com.cpchatbot.rest.model;
+
+public class Request {
+    public String messenger_user_id;
+}
